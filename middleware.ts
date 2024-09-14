08c360100aef8e8269f0cae9ce9f01bf0e3b1524
Hamza11hk <Hamzahk110077@gmail.com>
2024-09-14 23:17:11 +0100

    re-include the dashboard to the routes that need protection

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 export default clerkMiddleware((auth, req) => {
-  // Only protect routes that are NOT `/dashboard` and still need authentication
-  if (isProtectedRoute(req)) {
-    auth().protect();
-  }
-});
+    if (isProtectedRoute(req)) auth().protect();
+  });
 
-// Define which routes are protected ( i excluded `/dashboard` or any of its sub-routes)
 const isProtectedRoute = createRouteMatcher([
-  // I Added routes that need protection here, excluding /dashboard
-  '/((?!dashboard).*)', // Protect everything except /dashboard
-]);
+    '/dashboard(.*)',
+  ]);
+
+
+
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
